c2d9efb57abebd555706f80bedc3cfb0e06110c6
Dmitry Gusynin <d222a@yandex.ru>
2024-09-12 17:06:33 +0300

    Add json

--- test/test_CalcRating.py
+++ test/test_CalcRating.py
@@ -1,8 +1,11 @@
-from src.Types import DataType
-from src.CalcRating import CalcRating
+from src.Types import DataType, DataTypeJson
+from src.CalcRating import CalcRating, CalcRatingJson
 import pytest
+import json
 
 RatingsType = dict[str, float]
+
+RatingsTypeJson = dict[str, float]
 
 
 class TestCalcRating:
@@ -43,3 +46,45 @@ class TestCalcRating:
             rating_score = rating[student]
             assert pytest.approx(rating_score,
                                  abs=0.001) == input_data[1][student]
+
+
+class TestCalcRatingJson:
+    @pytest.fixture()
+    def input_data(self) -> tuple[DataTypeJson, RatingsTypeJson]:
+        # Представление данных в формате JSON
+        json_data = {
+            "Абрамов Петр Сергеевич": [
+                ("математика", 80),
+                ("русский язык", 76),
+                ("программирование", 100)
+            ],
+            "Петров Игорь Владимирович": [
+                ("математика", 61),
+                ("русский язык", 80),
+                ("программирование", 78),
+                ("литература", 97)
+            ]
+        }
+
+        # Конвертируем данные в JSON-строку
+        json_content = json.dumps(json_data, ensure_ascii=False)
+
+        # Ожидаемые рейтинговые оценки
+        rating_scores: RatingsTypeJson = {
+            "Абрамов Петр Сергеевич": 85.3333,
+            "Петров Игорь Владимирович": 79.0000
+        }
+
+        return json.loads(json_content), rating_scores
+
+    def test_init_calc_rating(self, input_data: tuple[DataTypeJson, RatingsTypeJson]) -> None:
+        # Инициализируем CalcRating и проверяем корректность данных
+        calc_rating = CalcRatingJson(input_data[0])
+        assert input_data[0] == calc_rating.dataJson
+
+    def test_calc(self, input_data: tuple[DataTypeJson, RatingsTypeJson]) -> None:
+        # Проверяем корректность расчета рейтингов
+        rating = CalcRatingJson(input_data[0]).calc()
+        for student in rating.keys():
+            rating_score = rating[student]
+            assert pytest.approx(rating_score, abs=0.001) == input_data[1][student]
--- test/test_TextDataReader.py
+++ test/test_TextDataReader.py
@@ -1,6 +1,9 @@
+from typing import Tuple, Dict, List
+
 import pytest
-from src.Types import DataType
-from src.TextDataReader import TextDataReader
+from src.Types import DataType, DataTypeJson
+from src.TextDataReader import TextDataReader, TextDataReaderJson
+import json
 
 
 class TestTextDataReader:
@@ -30,4 +33,42 @@ class TestTextDataReader:
 
     def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
         file_content = TextDataReader().read(filepath_and_data[0])
+        assert file_content == filepath_and_data[1]
+
+
+class TestDataReaderJson:
+    @pytest.fixture()
+    def json_data_content(self) -> tuple[str, DataTypeJson]:
+        json_data = {
+            "Иванов Константин Дмитриевич": {
+                "математика": 91,
+                "химия": 100
+            },
+            "Петров Петр Семенович": {
+                "русский язык": 87,
+                "литература": 78
+            }
+        }
+        json_content = json.dumps(json_data, ensure_ascii=False)
+        data = {
+            "Иванов Константин Дмитриевич": [
+                ("математика", 91), ("химия", 100)
+            ],
+            "Петров Петр Семенович": [
+                ("русский язык", 87), ("литература", 78)
+            ]
+        }
+        return json_content, data
+
+    @pytest.fixture()
+    def filepath_and_data(self,
+                          json_data_content: tuple[str, DataTypeJson],
+                          tmpdir) -> tuple[str, DataTypeJson]:
+        p = tmpdir.mkdir("datadir").join("my_data.json")
+        p.write_text(json_data_content[0], encoding='utf-8')
+        return str(p), json_data_content[1]
+
+    def test_read(self, filepath_and_data: Tuple[str, DataTypeJson]) -> None:
+        json_reader = TextDataReaderJson()
+        file_content = json_reader.read(filepath_and_data[0])
         assert file_content == filepath_and_data[1]
